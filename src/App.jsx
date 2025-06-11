a60aede7c2f442e5089feb783dea96a8748cab6a
ZAKMOUCHTATI44 <155732574+ZAKMOUCHTATI44@users.noreply.github.com>
2025-06-11 16:20:08 +0100

    change the home page

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,9 @@ function App() {
   console.log("App component loaded");
 
   return (
-    <Router>
-      <div className="font-sans"> 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/catalogue" element={<Catalogue />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogDetail />} />
-        </Routes>
-      </div>
-    </Router>
+   <div className="">
+
+   </div>
   );
 }
 
